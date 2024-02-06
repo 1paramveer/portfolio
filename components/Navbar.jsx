7bcee8d81c124f9d42919e3bdfdcd70ba53fc32e
Paramveer <128997056+1paramveer@users.noreply.github.com>
2024-02-06 23:28:53 +0530

    added clock to nav

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,10 +1,36 @@
+"use client";
 import React from "react";
 import Link from "next/link";
+
+const Clock = () => {
+  const [currentTime, setCurrentTime] = React.useState("00:00:00");
+
+  React.useEffect(() => {
+    const updateCurrentTime = () => {
+      const d = new Date();
+      const hours = String(d.getHours()).padStart(2, "0");
+      const minutes = String(d.getMinutes()).padStart(2, "0");
+      const seconds = String(d.getSeconds()).padStart(2, "0");
+      setCurrentTime(`${hours} : ${minutes} : ${seconds}`);
+    };
+
+    updateCurrentTime();
+
+    const timer = setInterval(updateCurrentTime, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  return <>{currentTime}</>;
+};
 
 const Navbar = () => {
   return (
     <nav className="flex justify-between max-lg:justify-center items-center h-1/4 px-20 text-sm">
       <div>
+        <div className="opacity-60 max-lg:hidden">
+          <Clock />
+        </div>
         <p className="enlarge-cursor font-sonder text-6xl text-twhite mt-2 text-center">
           Paramveer Singh
         </p>
