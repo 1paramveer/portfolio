f53b0fd87d8561875c49f6a2fe17bb6f39e4dbf3
Paramveer <128997056+1paramveer@users.noreply.github.com>
2024-02-07 17:54:20 +0530

    fixed viewport on sm devices

--- app/page.jsx
+++ app/page.jsx
@@ -8,7 +8,7 @@ export const metadata = {
 
 const page = () => {
   return (
-    <div className="w-1/3 font-jetbrains h-full flex flex-col justify-end max-lg:justify-center p-20 max-lg:w-full max-lg:pb-0 min-w-72">
+    <div className="w-1/3 font-jetbrains h-full flex flex-col justify-end max-lg:justify-center p-20 max-sm:p-10 max-lg:w-full max-lg:pb-0">
       <p className="mb-2">Work_</p>
       <div>
         <div className="flex justify-between">
