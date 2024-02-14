375addd3f3b35bac39a840550eaa1d2eeaaa92d0
Paramveer <128997056+1paramveer@users.noreply.github.com>
2024-02-15 00:26:50 +0530

    Revert "added client side rendering for each componenent"
    This reverts commit 9b6c9d6a9c4d14b42df490820f7050487981edd7.

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
+import gsap from "gsap";
 import "../app/globals.css";
 import { inter, sonder, dm_mono, dm_sans } from "../components/Fonts";
 import Cursor from "@/components/Cursor";
@@ -7,13 +9,21 @@ import HamburgerMenu from "@/components/HamburgerMenu";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
-export const metadata = {
-  title: "Paramveer Singh",
-};
-
 export default function RootLayout({ children }) {
+  useEffect(() => {
+    gsap.to(".text-split", {
+      y: "0%",
+      duration: 1,
+      stagger: 0.1,
+      delay: 0.2,
+      ease: "power2.out",
+    });
+  }, []);
   return (
     <html lang="en">
+      <head>
+        <title>Paramveer Singh</title>
+      </head>
       <body
         className={`${inter.variable} ${sonder.variable} ${dm_mono.variable} ${dm_sans.variable} flex flex-col overflow-hidden fixed h-full w-full`}
       >
