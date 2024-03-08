0434b76a4df89dd7342339b85227a5a09df42722
1paramveer <128997056+1paramveer@users.noreply.github.com>
2024-03-08 17:53:08 +0530

    added favicon

--- app/layout.js
+++ app/layout.js
@@ -14,19 +14,38 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <SmoothScroll>
-        <body
-          className={`${inter.variable} ${sonder.variable} ${dm_mono.variable} ${dm_sans.variable} `}
-        >
-          <Cursor />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+
+      <body
+        className={`${inter.variable} ${sonder.variable} ${dm_mono.variable} ${dm_sans.variable} `}
+      >
+        <Cursor />
+        <SmoothScroll>
           <div className="-mt-16 z-[10] fixed w-full flex justify-end">
             <HamburgerMenu />
           </div>
           <Navbar />
           <div className="">{children}</div>
           <Footer />
-        </body>
-      </SmoothScroll>
+        </SmoothScroll>
+      </body>
     </html>
   );
 }
