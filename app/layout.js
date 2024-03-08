657dba0b7ce5979a15a21b5c78a69db41a27fe9f
1paramveer <128997056+1paramveer@users.noreply.github.com>
2024-03-08 18:04:46 +0530

    fixes favicon

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Head from "next/head";
 import "../app/globals.css";
 import { inter, sonder, dm_mono, dm_sans } from "../components/Fonts";
 import Cursor from "@/components/Cursor";
@@ -14,7 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
+      <Head>
         <link
           rel="apple-touch-icon"
           sizes="180x180"
@@ -33,7 +34,7 @@ export default function RootLayout({ children }) {
           href="/favicon-16x16.png"
         />
         <link rel="manifest" href="/site.webmanifest" />
-      </head>
+      </Head>
 
       <body
         className={`${inter.variable} ${sonder.variable} ${dm_mono.variable} ${dm_sans.variable} `}
